bb973123939fe36c75b5cb6f45b899d5eaed36ff
Isha Sharma <ishasharma96@mail.fresnostate.edu>
2020-08-13 23:18:25 -0700

    first commit

---
+++ blog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+posts = [
+	{
+
+		'author': 'Isha Sharma',
+		'title': 'Blog Post 1',
+		'content': 'First post',
+		'date_posted': 'April 10, 2020'
+	},
+	{
+
+		'author': 'Ashutosh Sharma',
+		'title': 'Blog Post 2',
+		'content': 'GRE post',
+		'date_posted': 'August 10, 2020'
+	}
+]
+
+def home(request):
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
+
+def about(request):
+	return render(request, 'blog/about.html', {'title': 'About'})
+
+
+
+# Create your views here.
