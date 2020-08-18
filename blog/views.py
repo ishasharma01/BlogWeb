133bbd098ad160e5c3c8689080fa62f3a025d81b
Isha Sharma <ishasharma96@mail.fresnostate.edu>
2020-08-18 13:04:16 -0700

    User Registration

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-	{
-
-		'author': 'Isha Sharma',
-		'title': 'Blog Post 1',
-		'content': 'First post',
-		'date_posted': 'April 10, 2020'
-	},
-	{
-
-		'author': 'Ashutosh Sharma',
-		'title': 'Blog Post 2',
-		'content': 'GRE post',
-		'date_posted': 'August 10, 2020'
-	}
-]
-
 def home(request):
 	context = {
 		'posts': Post.objects.all()
